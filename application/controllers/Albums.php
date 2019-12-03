99b7095a2888293c1440f2ab4b9db0ef00fae12b
Tatenda Munenge <tatemunenge@gmail.com>
2019-12-03 13:25:39 +0200

    intergrated stipe client js

--- application/controllers/Albums.php
+++ application/controllers/Albums.php
@@ -14,17 +14,17 @@ class Albums extends CI_Controller {
 	}
 
 	public function view(){
-		$id = (int) $this->uri->segment(4);
+		$id = (int) $this->uri->segment(3);
 		if($id == 0){
 			redirect(base_url());
 		}
 		$lib = $this->load->library('albumsAdapter');
-		$data['albums'] = $this->albumsadapter->get_album($id);
-		
+		$data['album'] = $this->albumsadapter->get_album($id)[0];
 		$data['title'] = 'Albums | Music Shop';
-		$data['page'] = 'frontend/view_buy';
-		$data['css_array'] = array('store.css');
-		
-		//$this->load->view('frontend/view_layout',$data);
+		$data['page'] = 'frontend/view_view_album';
+		$data['css_array'] = array('view_album.css');
+		//$data['js_array'] = array('client.js');
+		//print_r($data['album'] );die();
+		$this->load->view('frontend/view_layout',$data);
 	}
 }
--- application/views/frontend/view_view_album.php
+++ application/views/frontend/view_view_album.php
@@ -1,7 +1,51 @@
-<div class="container">
-    <section>
-        <div class="row">
-           Buying
+<div class="container inner-content">
+    <div class="row">
+        <div class="col-sm">
+            <ul class="list-group">
+                <li class="list-group-item active">Album Details</li>
+                <li class="list-group-item"><?= $album['title']?></li>
+                <li class="list-group-item">$<?= $album['id']?></li>
+            </ul>
         </div>
-    </section>
+        <div class="col-sm">
+           <h4>Order Details</h4>
+           <table class='table'>
+                <thead>
+                    <tr>
+                        <th>Album</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Total</th>
+                    </tr>
+                <thead>
+                <tbody>
+                    <tr>
+                        <td><?= $album['title']?></td>
+                        <td>1</td>
+                        <td>$ <?= $album['id']?></td>
+                        <td>$ <?= $album['id']?></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>$ <?= $album['id']?></td>
+                    </tr>
+                </tbody>
+           </table>
+           <form action="/charge" method="post" id="payment-form" class='billable-class'>
+                <div class="form-row">
+                    <label for="card-element">
+                    Credit or debit card
+                    </label>
+                    <div id="card-element" class="form-control"></div>
+                    <div id="card-errors" role="alert"></div>
+                </div>
+                <br>
+                <button class='btn btn-primary'>Submit Payment</button>
+            </form>
+        </div>
+    </div>
 </div>
+<script src="https://js.stripe.com/v3/"></script>
+<script src="<?= base_url("assets/js/client.js")?>"></script>
