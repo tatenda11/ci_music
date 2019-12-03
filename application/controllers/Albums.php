1b2612807c4253a2f9b972334dd6994fb665f899
Tatenda Munenge <tatemunenge@gmail.com>
2019-12-03 10:15:48 +0200

    chenge Album to be a library instead of model

--- application/controllers/Albums.php
+++ application/controllers/Albums.php
@@ -8,6 +8,9 @@ class Albums extends CI_Controller {
 		$data['title'] = 'Albums | Music Shop';
 		$data['page'] = 'frontend/view_store';
 		$data['css_array'] = array('store.css');
+		$this->load->library('Albums','albums' );
+		$data['albums'] = $this->albums->get_albums();
+		print_r($data['albums']);
 		$this->load->view('frontend/view_layout',$data);
 	}
 }
